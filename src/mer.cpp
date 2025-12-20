76e07101a7fb1097b0635ba4e8173f8bf3ec0add
Erty <erty.gamedev@gmail.com>
2025-12-20 17:33:27 +0100

    Move BSP handling to MER files

--- src/mer.cpp
+++ src/mer.cpp
@@ -1,9 +1,8 @@
 #include <vector>
 #include <filesystem>
+#include "logging.h"
 #include "mer.h"
 #include "utils.h"
-#include "logging.h"
-#include "bspentreader.h"
 
 
 namespace fs = std::filesystem;
@@ -197,4 +196,72 @@ void Options::checkMaps()
     }
 
     std::cout << "\r\033[0K" << std::endl;
+}
+
+
+using namespace BSPFormat;
+
+
+Bsp::Bsp(const std::filesystem::path& filepath) {
+    m_filepath = filepath;
+    m_file.open(filepath, std::ios::binary);
+    if (!m_file.is_open() || !m_file.good())
+    {
+        m_file.close();
+        throw std::runtime_error("Could not open " + filepath.string());
+    }
+
+    m_file.read(reinterpret_cast<char*>(&m_header), sizeof(BspHeader));
+
+    if (m_header.version != 30)
+        throw std::runtime_error("Unexpected BSP version: " + std::to_string(m_header.version));
+
+    parse();
+    m_file.close();
+}
+
+void Bsp::parse()
+{
+    BspLump& entLump = m_header.lumps[LumpIndex::ENTITIES];
+
+    m_file.seekg(entLump.offset, std::ios::beg);
+    int lumpEnd = entLump.offset + entLump.length;
+    std::string lineBuffer;
+    lineBuffer.reserve(1024);
+
+    while (std::getline(m_file, lineBuffer) && m_file.tellg() < lumpEnd)
+    {
+        if (lineBuffer.starts_with('{'))
+        {
+            Entity entity = readEntity();
+
+        }
+    }
+}
+
+Entity Bsp::readEntity()
+{
+    Entity entity;
+
+    std::string lineBuffer;
+    lineBuffer.reserve(512);
+    while (std::getline(m_file, lineBuffer))
+    {
+        if (lineBuffer.starts_with('"'))
+        {
+            const auto& parts = splitString(lineBuffer, '"');
+            if (parts.size() != 4)
+                throw std::runtime_error("Invalid entity property: \"" + lineBuffer + "\"");
+
+            entity.insert_or_assign(parts.at(1), parts.at(3));
+            continue;
+        }
+
+        if (lineBuffer.starts_with('}'))
+            break;
+
+        throw std::runtime_error("Unexpected entity data: \"" + lineBuffer + "\"");
+    }
+
+    return entity;
 }
